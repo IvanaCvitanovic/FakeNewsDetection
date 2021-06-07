828aad44f9189ba5476253c251440618a60b8019
Ivana Cvitanović <ivana.cvi@gmail.com>
2021-06-07 15:52:58 +0200

    changes

--- model.py
+++ model.py
@@ -1,10 +1,86 @@
 # -*- coding: utf-8 -*-
 import torch
-from util.slidding_window import sliding_chunks_matmul_qk, mask_invalid_locations, sliding_chunks_matmul_pv, PositionalEncoding
 from torch import nn
 from typing import List
 import math
 import torch.nn.functional as F
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super().__init__()
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[: x.size(0), :]
+        return self.dropout(x)
+
+def sliding_chunks_matmul_qk(q: torch.Tensor, k: torch.Tensor, window: int, padding_value: float):
+  b, t, h, s = q.size()
+
+  assert t % window == 0
+  assert q.size() == k.size()
+  chunk_q = q.view(b, t // window, window, h, s)
+  chunk_k = k.view(b, t // window, window, h, s)
+  chunk_k_expanded = torch.stack((
+      F.pad(chunk_k[:, :-1], (0, 0, 0, 0, 0, 0, 1, 0), value=0.0),
+      chunk_k,
+      F.pad(chunk_k[:, 1:], (0, 0, 0, 0, 0, 0, 0, 1), value=0.0),
+  ), dim=-1)
+  diagonal_attn = torch.einsum('bcxhd,bcyhde->bcxhey', (chunk_q, chunk_k_expanded))
+  return diagonal_attn.reshape(b, t, h, 3 * window)
+
+
+
+def sliding_chunks_matmul_pv(prob: torch.Tensor, v: torch.Tensor, window: int):
+  b, t, h, s = v.size()
+  chunk_prob = prob.view(b, t // window, window, h, 3, window)
+  chunk_v = v.view(b, t // window, window, h, s)
+  chunk_v_expanded = torch.stack((
+        F.pad(chunk_v[:, :-1], (0, 0, 0, 0, 0, 0, 1, 0), value=0.0),
+        chunk_v,
+        F.pad(chunk_v[:, 1:], (0, 0, 0, 0, 0, 0, 0, 1), value=0.0),
+    ), dim=-1)
+  context = torch.einsum('bcwhpd,bcdhep->bcwhe', (chunk_prob, chunk_v_expanded))
+  return context.reshape(b, t, h, s)
+
+
+def invalid_locations_mask(window: int, device: str):
+  affected_seq_len = window
+  diagonals_list = []
+  for j in range(-window, 1):
+    diagonal_mask = torch.zeros(affected_seq_len, device='cpu', dtype=torch.uint8)
+    diagonal_mask[:-j] = 1
+    diagonals_list.append(diagonal_mask)
+  mask = torch.stack(diagonals_list, dim=-1)
+  mask = mask[None, :, None, :]
+  ending_mask = mask.flip(dims=(1, 3)).bool().to(device)
+  return affected_seq_len, mask.bool().to(device), ending_mask
+
+
+def mask_invalid_locations(input_tensor: torch.Tensor, window: int) -> torch.Tensor:
+  affected_seq_len, beginning_mask, ending_mask = invalid_locations_mask(window, input_tensor.device)
+  seq_len = input_tensor.size(1)
+  beginning_input = input_tensor[:, :affected_seq_len, :, :window+1]
+  beginning_mask = beginning_mask[:, :seq_len].expand(beginning_input.size())
+  beginning_input.masked_fill_(beginning_mask, -float('inf'))
+  ending_input = input_tensor[:, -affected_seq_len:, :, -(window+1):]
+  ending_mask = ending_mask[:, -seq_len:].expand(ending_input.size())
+  ending_input.masked_fill_(ending_mask, -float('inf'))
+  
+  
+
 
 class LongSelfAttention(nn.Module):
   def __init__(self, layer_id, attention_window: List[int] = None,  embedding_size = 100, num_heads = 8):
--- util.py
+++ util.py
@@ -33,23 +33,3 @@ def pad_to_window_size(input_ids: torch.Tensor,
     input_ids = F.pad(input_ids, (0, padding_len), value=pad_token_id)
     return input_ids
 
-class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super().__init__()
-
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
-        )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pe", pe)
-
-    def forward(self, x):
-        x = x + self.pe[: x.size(0), :]
-        return self.dropout(x)
